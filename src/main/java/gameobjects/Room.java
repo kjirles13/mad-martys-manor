6fab12bd103ad1fd90f6ccf53246bd94a9423015
Kit Jirles <kjirles13@gmail.com>
2023-02-20 13:24:32 -0500

    Add getters and setters to Room Class

--- src/main/java/gameobjects/Room.java
+++ src/main/java/gameobjects/Room.java
@@ -10,4 +10,36 @@ public class Room extends Thing {
         this.e = e;
         this.w = w;
     }
+
+    public int getN() {
+        return n;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
 }
