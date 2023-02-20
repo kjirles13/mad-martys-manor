d1550b94130f66570714648b29ca8549d6a91ac5
Kit Jirles <kjirles13@gmail.com>
2023-02-20 17:11:53 -0500

    Add Grand Staircase

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -17,7 +17,7 @@ public class Game {
                 -1, -1, -1, 1));
         map.add(new Room("Foyer",
                 "A large front hall with high ceilings, a grand chandelier, and an even\ngrander main staircase. An ominous old wooden clock sits to the left,\nand its swinging pendulum sounds like the heartbeat of this manor...\ntick...tick...tick...",
-                2, -1, 0, 3));
+                -2, -1, 0, 3));
         map.add(new Room("Grand Staircase",
                 "",
                 -1, -1, -1, -1));
@@ -27,23 +27,44 @@ public class Game {
 
 
         enterRoom(1);
+        enterRoom(-2);
     }
 
     public static void enterRoom(int roomIndex) {
-        if (roomIndex < 0) {
+        if (roomIndex == -1) {
             System.out.println("You can't go that way!");
             return;
+        } else if (roomIndex == -2) {
+            roomIndex = 12;
+            System.out.println("You're walking up the spiral staircase.");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        } else if (roomIndex == -3) {
+            roomIndex = 1;
+            System.out.println("You're walking down the Grand Staircase.");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         }
 
         Room room = map.get(roomIndex);
         player.setCurrentRoom(room);
 
         System.out.println("You're in the " + room.getName());
+
         try {
             Thread.sleep(1000);
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
+
         System.out.println(room.getDescription());
     }
 }
