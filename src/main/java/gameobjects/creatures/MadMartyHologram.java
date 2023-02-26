a925980a64b6b3b0dc85476731c04c516adbde84
Kit Jirles <kjirles13@gmail.com>
2023-02-26 17:58:10 -0500

    Add MadMartyHologram

---
+++ src/main/java/gameobjects/creatures/MadMartyHologram.java
@@ -0,0 +1,15 @@
+package gameobjects.creatures;
+
+import gameobjects.Thing;
+
+public class MadMartyHologram extends Thing {
+
+    public MadMartyHologram(String name, String description) {
+        super(name, description);
+    }
+
+    public String getIntro() {
+        String intro = "";
+        return intro;
+    }
+}
