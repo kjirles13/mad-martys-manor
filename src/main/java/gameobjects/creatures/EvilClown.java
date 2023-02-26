20d6c7a819858d4b20883e196fa512e1d48e73f7
Kit Jirles <kjirles13@gmail.com>
2023-02-26 18:31:12 -0500

    Add creature EvilClown

---
+++ src/main/java/gameobjects/creatures/EvilClown.java
@@ -0,0 +1,13 @@
+package gameobjects.creatures;
+
+import gameobjects.Thing;
+
+public class EvilClown extends Thing {
+    public EvilClown(String name, String description) {
+        super(name, description);
+    }
+
+    public String move() {
+        return "";
+    }
+}
