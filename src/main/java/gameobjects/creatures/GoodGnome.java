77f0a842b1bfa5854e2cbb2d485cf1d881d077d3
Kit Jirles <kjirles13@gmail.com>
2023-02-26 18:39:52 -0500

    Add GoodGnome to creatures package

---
+++ src/main/java/gameobjects/creatures/GoodGnome.java
@@ -0,0 +1,9 @@
+package gameobjects.creatures;
+
+import gameobjects.Thing;
+
+public class GoodGnome extends Thing {
+    public GoodGnome(String name, String description) {
+        super(name, description);
+    }
+}
