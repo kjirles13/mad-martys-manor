66d0a220eddb7c63e02c0a878f1afb34004cf3ec
Kit Jirles <kjirles13@gmail.com>
2023-02-26 18:37:38 -0500

    Add EvilGnome class to creatures package

---
+++ src/main/java/gameobjects/creatures/EvilGnome.java
@@ -0,0 +1,9 @@
+package gameobjects.creatures;
+
+import gameobjects.Thing;
+
+public class EvilGnome extends Thing {
+    public EvilGnome(String name, String description) {
+        super(name, description);
+    }
+}
