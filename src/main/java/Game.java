3e64e954162d23299136c7a45967f75ff20a4c57
Bryce Huhtala <sketchius@gmail.com>
2023-02-26 22:23:41 -0500

    Add custom string parser with demo code

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,6 @@
 import gameobjects.Player;
 import gameobjects.Room;
+import gameobjects.utility.StringParser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,14 +30,15 @@ public class Game {
 
 
 
+        print("You hear the clock...~ tick...~ tick...~ tick...");
+//        print("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non cursus augue, eget maximus urna. Donec fringilla urna a sem accumsan, nec dapibus mi auctor. Etiam ante ex, elementum in justo a, maximus gravida eros. Aliquam lacinia, libero a porttitor hendrerit, ligula erat convallis enim, in accumsan augue ante et est. Duis varius nec tortor eu lacinia. Aenean non nisi sit amet nisi pharetra tempor. Vestibulum maximus mauris ac hendrerit aliquet. Fusce ornare faucibus orci. Donec imperdiet mi eu nibh accumsan, sed euismod sem pharetra. Mauris non eros eget leo tempor fermentum nec id arcu. In risus libero, consequat at turpis et, lobortis pretium velit. Mauris pellentesque nulla id arcu dictum, fermentum iaculis neque facilisis. Morbi vehicula diam quis leo elementum sagittis. Integer ultrices, neque sagittis consectetur efficitur, nisl nisi eleifend sem, non dictum est dolor id sem.");
 
-        map.get(1).setPhrase("Back in the foyer. Man, that clock gives me the creeps...");
+//        map.get(1).setPhrase("Back in the foyer. Man, that clock gives me the creeps...");
+//
+//
+//        enterRoom(1);
+//        enterRoom(-2);
 
-
-        enterRoom(1);
-        enterRoom(-2);
-
-//        print("You hear the clock...~ tick...~ tick...~ tick...");
     }
 
     public static void enterRoom(int roomIndex) {
@@ -79,7 +81,8 @@ public class Game {
 
 
     private static void print(String string) {
-        String[] sections = string.split("~");
+//        String[] sections = string.split("~");
+        String[] sections = StringParser.parse(string,60);
         for (int i = 0; i < sections.length; i++) {
             System.out.print(sections[i]);
             if (i+1 < sections.length) {
